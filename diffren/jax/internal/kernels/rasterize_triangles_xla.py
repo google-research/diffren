22927a893fc0462e90ca9f2b08ca8c09eb0ccf3a
Peter Hawkins <phawkins@google.com>
2023-09-20 07:41:00 -0700

    [JAX] Migrate XlaBuilder users to emit direct stablehlo MLIR lowerings.
    Change in preparation for removing XlaBuilder and classic HLO IR from jaxlib.
    
    No functional changes are intended.
    
    PiperOrigin-RevId: 566960515

--- diffren/jax/internal/kernels/rasterize_triangles_xla.py
+++ diffren/jax/internal/kernels/rasterize_triangles_xla.py
@@ -21,6 +21,7 @@ from . import rasterize_triangles_gpu
 from diffren.jax.internal.kernels import descriptors_pb2
 import jax
 from jax.interpreters import batching
+from jax.interpreters import mlir
 import jax.numpy as jnp
 import numpy as np
 
@@ -35,108 +36,79 @@ for _name, _value in rasterize_triangles_gpu.registrations().items():
   xla_client.register_custom_call_target(_name, _value, platform='gpu')  # pytype: disable=module-attr
 
 
-def _get_shape(builder, element):
-  return xla_client.Shape.array_shape(
-      builder.GetShape(element).numpy_dtype(),
-      builder.GetShape(element).dimensions(),
-      list(reversed(range(len(builder.GetShape(element).dimensions())))))
+def _major_to_minor_layout(shape):
+  return tuple(range(len(shape) - 1, -1, -1))
 
 
-def _translation_rule_cpu(builder, vertices, triangles, *, image_width,
+def _translation_rule_cpu(ctx, vertices, triangles, *, image_width,
                           image_height, num_layers, face_culling_mode):
   """Rasterize triangles translation rule for CPU using XLA."""
+  vertices_aval, triangles_aval = ctx.avals_in
 
-  def get_int_op_and_shape(value):
-    operand = xops.Constant(builder, np.int32(value))
-    shape = _get_shape(builder, operand)
-    return operand, shape
+  def get_int_op_and_layout(value):
+    value = np.array(value, dtype=np.int32)
+    operand = mlir.ir_constant(value)
+    layout = _major_to_minor_layout(value.shape)
+    return operand, layout
 
-  def get_count_op_and_shape(array):
-    operand = xops.Constant(builder,
-                            np.int32(builder.GetShape(array).dimensions()[0]))
+  def get_count_op_and_layout(array_shape):
+    operand = mlir.ir_constant(np.array(array_shape[0], dtype=np.int32))
+    return operand, ()
 
-    shape = _get_shape(builder, operand)
-    return operand, shape
+  vertices_layout = _major_to_minor_layout(vertices_aval.shape)
+  triangles_layout = _major_to_minor_layout(triangles_aval.shape)
+  vertex_count_operand, vertex_count_layout = get_count_op_and_layout(
+      vertices_aval.shape)
+  triangle_count_operand, triangle_count_layout = get_count_op_and_layout(
+      triangles_aval.shape)
 
-  vertices_shape = _get_shape(builder, vertices)
-  triangles_shape = _get_shape(builder, triangles)
-  vertex_count_operand, vertex_count_shape = get_count_op_and_shape(vertices)
-  triangle_count_operand, triangle_count_shape = get_count_op_and_shape(
-      triangles)
-
-  height_operand, height_shape = get_int_op_and_shape(image_height)
-  width_operand, width_shape = get_int_op_and_shape(image_width)
-  num_layers_operand, num_layers_shape = get_int_op_and_shape(num_layers)
-  culling_mode_operand, culling_mode_shape = get_int_op_and_shape(
+  height_operand, height_layout = get_int_op_and_layout(image_height)
+  width_operand, width_layout = get_int_op_and_layout(image_width)
+  num_layers_operand, num_layers_layout = get_int_op_and_layout(num_layers)
+  culling_mode_operand, culling_mode_layout = get_int_op_and_layout(
       face_culling_mode)
 
-  operand_shapes = [
-      vertices_shape, vertex_count_shape, triangles_shape, triangle_count_shape,
-      width_shape, height_shape, num_layers_shape, culling_mode_shape
-  ]
-
-  triangle_ids_shape = xla_client.Shape.array_shape(
-      np.dtype(np.int32), (num_layers, image_height, image_width), [2, 1, 0])
-  z_buffer_shape = xla_client.Shape.array_shape(
-      np.dtype(np.float32), (num_layers, image_height, image_width), [2, 1, 0])
-  barycentrics_shape = xla_client.Shape.array_shape(
-      np.dtype(np.float32), (num_layers, image_height, image_width, 3),
-      [3, 2, 1, 0])
-  output_shapes = xla_client.Shape.tuple_shape(
-      [triangle_ids_shape, z_buffer_shape, barycentrics_shape])
-
-  out = xops.CustomCallWithLayout(
-      builder,
-      b'rasterize_triangles',
+  return mlir.custom_call(
+      'rasterize_triangles',
       operands=[
           vertices, vertex_count_operand, triangles, triangle_count_operand,
           width_operand, height_operand, num_layers_operand,
           culling_mode_operand
       ],
-      operand_shapes_with_layout=operand_shapes,
-      shape_with_layout=output_shapes,
+      operand_layouts=[
+          vertices_layout, vertex_count_layout, triangles_layout,
+          triangle_count_layout, width_layout, height_layout, num_layers_layout,
+          culling_mode_layout
+      ],
+      result_types=[mlir.aval_to_ir_type(aval) for aval in ctx.avals_out],
+      result_layouts=[[2, 1, 0], [2, 1, 0], [3, 2, 1, 0]],
       api_version=xla_client.ops.CustomCallApiVersion
-      .API_VERSION_STATUS_RETURNING)
-  return out
+      .API_VERSION_STATUS_RETURNING).results
 
 
-def _translation_rule_gpu(builder, vertices, triangles, *, image_width,
+def _translation_rule_gpu(ctx, vertices, triangles, *, image_width,
                           image_height, num_layers, face_culling_mode):
   """Rasterize triangles translation rule for GPU (CUDA) using XLA."""
-
-  vertices_shape = _get_shape(builder, vertices)
-  triangles_shape = _get_shape(builder, triangles)
-
-  triangle_ids_shape = xla_client.Shape.array_shape(
-      np.dtype(np.int32), (num_layers, image_height, image_width), [2, 1, 0])
-  z_buffer_shape = xla_client.Shape.array_shape(
-      np.dtype(np.float32), (num_layers, image_height, image_width), [2, 1, 0])
-  barycentrics_shape = xla_client.Shape.array_shape(
-      np.dtype(np.float32), (num_layers, image_height, image_width, 3),
-      [3, 2, 1, 0])
-  output_shapes = xla_client.Shape.tuple_shape(
-      [triangle_ids_shape, z_buffer_shape, barycentrics_shape])
+  vertices_aval, triangles_aval = ctx.avals_in
 
   descriptor = descriptors_pb2.RasterizeTrianglesConfig()
   descriptor.image_width = image_width
   descriptor.image_height = image_height
   descriptor.num_layers = num_layers
   descriptor.face_culling_mode = face_culling_mode
-  descriptor.triangle_count = triangles_shape.dimensions()[0]
+  descriptor.triangle_count = triangles_aval.shape[0]
 
-  out = xops.CustomCallWithLayout(
-      builder,
-      b'rasterize_triangles',
-      operands=[
-          vertices,
-          triangles,
-      ],
-      operand_shapes_with_layout=[vertices_shape, triangles_shape],
-      shape_with_layout=output_shapes,
-      opaque=descriptor.SerializeToString(),
+  return mlir.custom_call(
+      'rasterize_triangles',
+      operands=[vertices, triangles],
+      operand_layouts=[_major_to_minor_layout(vertices_aval.shape),
+                       _major_to_minor_layout(triangles_aval.shape)],
+      result_types=[mlir.aval_to_ir_type(aval) for aval in ctx.avals_out],
+      result_layouts=[[2, 1, 0], [2, 1, 0], [3, 2, 1, 0]],
+      backend_config=descriptor.SerializeToString(),
       api_version=xla_client.ops.CustomCallApiVersion
-      .API_VERSION_STATUS_RETURNING)
-  return out
+      .API_VERSION_STATUS_RETURNING
+  ).results
 
 
 def rasterize_triangles_abstract_eval(vertices, triangles, *, image_width,
@@ -163,10 +135,12 @@ rasterize_triangles_p.def_impl(functools.partial(
     jax.interpreters.xla.apply_primitive, rasterize_triangles_p))
 rasterize_triangles_p.def_abstract_eval(rasterize_triangles_abstract_eval)
 
-jax.interpreters.xla.backend_specific_translations['cpu'][
-    rasterize_triangles_p] = _translation_rule_cpu
-jax.interpreters.xla.backend_specific_translations['gpu'][
-    rasterize_triangles_p] = _translation_rule_gpu
+mlir.register_lowering(
+    rasterize_triangles_p, _translation_rule_cpu, platform='cpu'
+)
+mlir.register_lowering(
+    rasterize_triangles_p, _translation_rule_gpu, platform='cuda'
+)
 
 
 def rasterize_triangles(vertices, triangles, image_width, image_height,
